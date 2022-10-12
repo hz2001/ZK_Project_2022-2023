f4d85307679520bee4d7a1cded275bdb805b200f
scarzhang01 <katzhang@bu.edu>
2022-10-11 21:16:21 -0400

    new working file

--- Kangning/stringlist_search.py
+++ Kangning/stringlist_search.py
@@ -1,5 +1,3 @@
-import pprint
-import random
 import sys
 import functools
 from miniwizpl import SecretInt, SecretList, mux, public_foreach, print_emp
@@ -7,10 +5,6 @@ from miniwizpl import SecretInt, SecretList, mux, public_foreach, print_emp
 if len(sys.argv) != 2:
     print("Usage: python dfa_example.py <target_filename>")
     sys.exit()
-
-# the accept state is 1000000
-accept_state = 1000000
-
 
 def word_to_integer(word):
     hash = 0
@@ -21,10 +15,11 @@ def word_to_integer(word):
     return hash
 
 
-# a Python function to create a dfa from a string
-# we assume a default transition back to 0
+accept_state = 255
+
+
 def islast(i, word):
-    if len(word) - 1 == i:
+    if len(word.split()) - 1 == i:
         return True
     else:
         return False
@@ -35,6 +30,7 @@ def equals(pointer, state):
         if pointer[i] != state[i]:
             return False
     return True
+
 
 
 def dfa_from_string(stringlist):
@@ -52,7 +48,7 @@ def dfa_from_string(stringlist):
         counter = len(states)
         for word in wordlist:
             pointer = state.copy()
-            for i in range(length):  # 0,1,2
+            for i in range(length): # 0,1,2
                 if state[i] != accept_state:
                     if word == stringlist[i].split()[state[i]]:
                         if islast(state[i], stringlist[i]):
@@ -70,7 +66,6 @@ def dfa_from_string(stringlist):
             finished = True
     return next_state
 
-
 strings_present = [
     'import socket',
     'import numpy',
@@ -82,44 +77,68 @@ strings_not_present = [
     'import socket'
 ]
 
-# read the target file & convert to secret string
-with open(sys.argv[1], 'r') as f:
+with open("test.txt", 'r') as f:
     file_data = f.read()
+
 
 file_data = file_data.split()
 file_string = SecretList([word_to_integer(c) for c in file_data])
 
+dfa = dfa_from_string(strings_present)
 
-# run a dfa
-def run_dfa(dfa, string, len):
+# The helper function for comparing two state values
+def stateCal(s):
+    result = 0
+    for i in range(len(s)):
+        result += (s[i]<<8*i)
+    return result
+
+
+def stateComp(state1, state2):
+    if stateCal(state1) == stateCal(state2):
+        return True
+    else:
+        return False
+
+accept = tuple([255]*len(strings_present))
+accept = stateCal(accept)
+zero_state = tuple([0]*len(strings_present))
+zero_state = stateCal(zero_state)
+
+def run_dfa(dfa, string):
     def next_state_fun(word, state):
-        output = [0]*len
-        accept_state_final = tuple([accept_state]*len)
+        output = zero_state
+
         for (dfa_state, dfa_word), next_state in dfa.items():
-            output = mux((state == dfa_state) & (word == dfa_word),  # can compare tuples?
+            # transform all tuples to numbers
+            dfa_state = stateCal(dfa_state)
+            next_state = stateCal(next_state)
+            output = mux(state == dfa_state & (word == dfa_word),
                          next_state,
                          output)
 
-        output = mux(state == accept_state_final, accept_state_final, output)
+        output = mux(state == accept, accept, output)
         return output
 
     return public_foreach(string,
                           next_state_fun,
-                          0)
+                          zero_state)
 
+'''
+def public_foreach(ls, fn, init):
+    accumulator = init
+    for x in ls:
+        accumulator = fn(x, accumulator)
+    return accumulator
+'''
 
 # define the ZK statement
-outputs = []
 
-dfa = dfa_from_string(strings_present)
-outputs.append(run_dfa(dfa, file_string, len(strings_present)) == accept_state)
-# will output accept when all is found
-
-
-dfa = dfa_from_string(strings_not_present) # maybe not
-outputs.append(run_dfa(dfa, file_string) != accept_state)
+outputs = (run_dfa(dfa, file_string) == accept)
 
 print(outputs)
+
 # compile the ZK statement to an EMP file
+# TODO: what reduce is doing here?
 output = functools.reduce(lambda a, b: a & b, outputs)
 print_emp(output, 'miniwizpl_test.cpp')
